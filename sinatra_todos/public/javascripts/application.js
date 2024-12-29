51fa6273ee077c2cf900e9c93103f3954be12d03
Hugo Ma <huwgoma@gmail.com>
2024-12-29 14:30:17 -0500

    Add confirmation prompt for deleting to-dos and lists

--- sinatra_todos/public/javascripts/application.js
+++ sinatra_todos/public/javascripts/application.js
@@ -1,1 +1,14 @@
-console.log("This is a test.");
+$(function(){
+  // Confirm delete for to-dos and lists
+  $('form.delete').on('click', function(event){
+    // Prevent default HTML behavior (form being submitted) from occurring 
+    event.preventDefault();
+    // Stop the event from propagating (cut the chain of events)
+    event.stopPropagation();
+
+    if (confirm("Are you sure? This cannot be undone.")) {
+      // Submit manually only if condition is met
+      $(this).submit();
+    }
+  })
+});
