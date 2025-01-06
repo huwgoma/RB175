7f8de35bc6ac08f76f0c588f2d9a37f24322dd71
Hugo Ma <huwgoma@gmail.com>
2025-01-06 10:50:05 -0500

    Add content to data/ files and update index links to display each file as plaintext

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -6,12 +6,21 @@ if development?
 end
 
 before do
-  
+  @root = File.expand_path("..", __FILE__)
 end
 
 get '/' do
-  app_root = File.expand_path("..", __FILE__)
-  @file_names = Dir.children("#{app_root}/data")
-  
+  @file_names = Dir.children("#{@root}/data")
+
   erb :files
+end
+
+get '/:file_name' do
+  file_name = params[:file_name]
+  file_path = "#{@root}/data/#{file_name}"
+  
+  headers['Content-Type'] = 'text/plain'
+  @file = File.read(file_path)
+  # Render contents of file
+  # (Direct serving? - only for public?)
 end
