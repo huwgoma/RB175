5f6ab118c271a9bf6d8d51710ab06650a910c451
Hugo Ma <huwgoma@gmail.com>
2025-01-07 17:05:06 -0500

    Add ability for users to log in/out of the application. Pending refactor to show files even if the user is not logged in.

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -15,16 +15,91 @@ before do
 
 end
 
+helpers do
+  def logged_in?
+    session[:logged_in]
+  end
+end
+# Signing in and out:
+# 1) When a signed-out user views the index page, they should see a sign in link:
+# [Sign In]    
+#  => Routes to GET /users/signin
+#  
+# 2) The signin page contains a form with 3 elements:
+# [Username]
+# [Password]
+# [Sign In]
+#  => Submits to POST /users/signin
+#  
+# 3) POST /users/signin
+#  - Verify the given params[:username] and params[:password]
+#    - If credentials are valid (username == 'admin' and password == 'secret'),
+#      set a session message ('Welcome!') and redirect the user to the index page.
+#      - In addition, store the following in the session: 
+#        - username
+#        - signed_in = true
+
+#    - If credentials are invalid, set a session message ('Invalid credentials')
+#      and re-render the signin form.
+#      - username value should be params[:username] (the previously-entered value)
+#      
+# 4) GET /
+#    - If the user is not signed in, display the login page.
+#    - If the user is signed in, display the files page as normal.
+#      - Also add a link at the bottom:
+#      Signed in as <USERNAME>. [Sign Out]
+#                                => Submits to POST /users/logout
+# 5) POST /users/logout
+#    - Delete the username from the session
+#    - Set session[logged_in] to false
+#    - Set a session message ('You have been signed out.')
+#    - Redirect to index (/)
+
+
+
 # # # # # # 
 # Routes  #
 # # # # # # 
+
+# User login form
+get '/users/login' do
+  redirect '/' if logged_in?
+  erb :login  
+end
+
+# User login
+post '/users/login' do
+  if valid_login?(params[:username], params[:password])
+    session[:message] = 'Welcome!'
+    session[:username] = params[:username]
+    session[:logged_in] = true
+    redirect '/'
+  else
+    session[:message] = 'Invalid login credentials.'
+    erb :login
+  end
+end
+
+post '/users/logout' do
+  session.delete(:username)
+  session[:logged_in] = false
+  session[:message] = 'You have been logged out.'
+  
+  redirect '/'
+end
+
+def valid_login?(username, password)
+  username == 'admin' && password == 'secret'
+end
+
 # Home Page - Display all files
 get '/' do
-  @file_names = Dir.glob("#{data_path}/*").map do |path|
-    File.basename(path)
+  if logged_in?
+    @file_names = Dir.glob("#{data_path}/*").map { |path| File.basename(path) }
+    @username = session[:username]
   end
 
-  erb :files
+  erb :index
 end
 
 # Form to create new files 
